6921dfef448eafb2d81245171c2d44c4e09bd11d
w0xter <luispozogilo@gmail.com>
2020-01-20 13:59:44 +0100

    Fixing Bugs

--- data.js
+++ data.js
@@ -1,3 +1,26 @@
 function getMadrid(){
+    let data = httpGet('https://raw.githubusercontent.com/oeg-upm/pausa-web/master/madrid.distritos.geojson.json');
+    console.log(data);
+    console.log(JSON.parse(data));
+    return JSON.parse(data);
+}
+function getParques(){
+  let data = httpGet('https://raw.githubusercontent.com/oeg-upm/pausa-web/master/ENP_CMadrid.geojson.js');
+  console.log(data);
+  console.log(JSON.parse(data));
+  return JSON.parse(data);
 
+}
+function getComarcas(){
+  let data = httpGet('https://raw.githubusercontent.com/oeg-upm/pausa-web/master/ComarcasAgrariasCM.geojson.js');
+  console.log(data);
+  console.log(JSON.parse(data));
+  return JSON.parse(data);
+
+}
+function httpGet(theUrl){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
 }
